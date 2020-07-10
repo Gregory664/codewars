e1f99df6f162ce45c277b03fc74a080af492d94d
gregory664 <gregory664@yandex.ru>
2020-07-10 14:24:23 +0300

    complete task: STRONGN Strong Number (Special Numbers Series #2)
    src: https://www.codewars.com/kata/5a4d303f880385399b000001

---
+++ src/main/java/codewars/kyu7/StrongNumber.java
@@ -0,0 +1,14 @@
+package codewars.kyu7;
+
+import java.util.stream.IntStream;
+
+public class StrongNumber {
+    public static String isStrongNumber(int num) {
+        return num == String.valueOf(num).chars()
+                .map(Character::getNumericValue)
+                .map(i -> IntStream.rangeClosed(1, i).reduce(1, (x, y) -> x * y))
+                .sum() ? "STRONG!!!!" : "Not Strong !!";
+    }
+
+
+}
