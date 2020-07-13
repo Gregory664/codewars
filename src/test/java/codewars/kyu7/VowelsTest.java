69aaeae4719cbc233732d4dcf5548f3c1f97ab70
gregory664 <gregory664@yandex.ru>
2020-07-13 15:30:55 +0300

    complete task: Vowel Count
    src: https://www.codewars.com/kata/54ff3102c1bad923760001f3

---
+++ src/test/java/codewars/kyu7/VowelsTest.java
@@ -0,0 +1,12 @@
+package codewars.kyu7;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class VowelsTest {
+    @Test
+    public void testCase1() {
+        assertEquals("Nope!", 5, Vowels.getCount("abracadabra"));
+    }
+}
