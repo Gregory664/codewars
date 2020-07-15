346fbc48a523efb9c1a38d71e7b2c842852adfbd
gregory664 <gregory664@yandex.ru>
2020-07-15 17:52:04 +0300

    complete task: Growth of a Population
    src: https://www.codewars.com/kata/563b662a59afc2b5120000c6

---
+++ src/main/java/codewars/kyu7/GrowthOfAPopulation.java
@@ -0,0 +1,12 @@
+package codewars.kyu7;
+
+public class GrowthOfAPopulation {
+    public static int nbYear(int p0, double percent, int aug, int p) {
+        int count = 0;
+        while (p0 < p) {
+            p0 += (p0 * percent / 100) + aug;
+            count++;
+        }
+        return count;
+    }
+}
