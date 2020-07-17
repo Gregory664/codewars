fc2ccec08aa02afbbba9e65692f3a3e4fcd9733c
gregory664 <gregory664@yandex.ru>
2020-07-17 15:50:07 +0300

    complete Money, Money, Money
    src: https://www.codewars.com/kata/563f037412e5ada593000114

---
+++ src/main/java/codewars/kyu7/Money.java
@@ -0,0 +1,12 @@
+package codewars.kyu7;
+
+public class Money {
+    public static int calculateYears(double principal, double interest,  double tax, double desired) {
+        int years = 0;
+        while (principal < desired) {
+            principal += (principal * interest) - (principal * interest * tax);
+            years++;
+        }
+        return years;
+    }
+}
