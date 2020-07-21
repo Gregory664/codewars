8cf88644f791cf2b94fc1c5ae9c6f55b5d3b2e09
gregory664 <gregory664@yandex.ru>
2020-07-21 10:27:33 +0300

    complete Between Extremes
    src: https://www.codewars.com/kata/56d19b2ac05aed1a20000430

---
+++ src/test/java/codewars/kyu7/BetweenExtremesTest.java
@@ -0,0 +1,13 @@
+package codewars.kyu7;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BetweenExtremesTest {
+    @Test
+    public void fixedTest() {
+        assertEquals(42, BetweenExtremes.betweenExtremes(new int[]{21, 34, 54, 43, 26, 12}));
+        assertEquals(99, BetweenExtremes.betweenExtremes(new int[]{-1, -41, -77, -100}));
+    }
+}
