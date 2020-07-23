b557ee3a6f35527d79e9ad67a97a7b628266c8ff
gregory664 <gregory664@yandex.ru>
2020-07-23 09:26:29 +0300

    complete Credit Card Mask
    src: https://www.codewars.com/kata/5412509bd436bd33920011bc

---
+++ src/main/java/codewars/kyu7/Maskify.java
@@ -0,0 +1,14 @@
+package codewars.kyu7;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Maskify {
+    public static String maskify(String str) {
+        return str.length() > 4 ?
+                IntStream.range(0, str.length() - 4)
+                        .mapToObj(i -> "#")
+                        .collect(Collectors.joining()) + str.substring(str.length() - 4) : str;
+
+    }
+}
